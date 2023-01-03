80d16e4c0a239bae11d808b7465ad490df693252
zainhanif73 <www.zainhanif73@gmail.com>
2023-01-03 15:06:29 +0500

    fix:(Sidebar Design)

---
+++ src/Style/Sidebar.js
@@ -0,0 +1,7 @@
+const color = {
+    redColor:"flex text-white cursor-pointer text-[15px] mb-8 bg-[#FC1503] pb-2 rounded-full",
+    simpleColor:"flex text-white cursor-pointer text-[15px] mb-8",
+    textColor:"text-white w-[30px] ml-3"
+}
+
+export default color;
