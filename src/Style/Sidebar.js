351772fe3f63dd98d6de22eb7736aadb90ebcf42
zainhanif73 <www.zainhanif73@gmail.com>
2023-01-05 13:01:10 +0500

    fix:(Design Issue)

--- src/Style/Sidebar.js
+++ src/Style/Sidebar.js
@@ -1,5 +1,5 @@
 const color = {
-    redColor:"flex text-white cursor-pointer text-[15px] mb-8 bg-[#FC1503] pr-[25px] md:pr-0 pb-2 h-[40px] md:h-0 rounded-full",
+    redColor:"flex text-white cursor-pointer text-[15px] mb-8 bg-[#FC1503] pr-[25px] md:pr-0 pb-2 h-[40px] rounded-full",
     simpleColor:"flex text-white cursor-pointer text-[15px] mb-8 pr-2",
     textColor:"text-white w-[30px] ml-3"
 }
