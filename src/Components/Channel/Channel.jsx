2b6f6bc378c503fdab1bc51dc4a4676b789c576d
zainhanif73 <www.zainhanif73@gmail.com>
2023-01-10 14:22:59 +0500

    design:(Changed)

--- src/Components/Channel/Channel.jsx
+++ src/Components/Channel/Channel.jsx
@@ -64,20 +64,20 @@ function Channel() {
                         <div className='w-[auto] mt-8 ml-16' key={channel.items[0].snippet.thumbnails.high.url}>
                             <img className='rounded-full' src={channel.items[0].snippet.thumbnails.high.url} alt="" style={{ width: "182px", height: "182px" }} />
                             <div className='text-[#ffffff] p-6 font-[600]'>
-                                <div className='mt-4 flex cursor-pointer' ><a className='flex' target={"_blank"} href={"https://www.youtube.com/" + channel.items[0].snippet.title}>{channel.items[0].snippet.title} <span className='ml-2 pt-1'><Tick /></span></a></div>
+                                <div className='mt-4 flex cursor-pointer' ><div className='flex' onClick={()=>{navigate("/channel/"+channel.items[0].id)}}>{channel.items[0].snippet.title} <span className='ml-2 pt-1'><Tick /></span></div></div>
                             </div>
                         </div>
                     </div>
                 </div>
             }
-            <div className='flex flex-wrap justify-center mt-32'>
+            <div className='flex flex-wrap justify-center mt-32 pt-16'>
                 {data && data.length &&
                     data?.map((data,index) =>
-                        <div className='flex flex-col w-[300px] ml-8 mt-2' key={data.snippet.channelId+index}>
-                            <div className='h-[230px] text-white' style={{ backgroundImage: `url(${data.snippet.thumbnails.high.url})` }}></div>
+                        <div className='flex flex-col cursor-pointer w-[300px] ml-8 mt-2' key={data.snippet.channelId+index}>
+                            <div className='h-[180px] text-white' style={{ backgroundSize:'cover',backgroundPosition:'center center' ,backgroundRepeat:'no-repeat',backgroundImage: `url(${data.snippet.thumbnails.high.url})` }}></div>
                             {/* <img src={data.snippet.thumbnails.high.url} alt="" style={{ width: "480px", height: "360px" }} /> */}
-                            <div className='bg-[#1e1e1e] text-[#ffffff] min-h-[180px] h-fit p-6 font-[600]'>
-                                <span><div className='hover:cursor-pointer' onClick={() => { navigate("/videoes/" + data.snippet.channelId + "+" + data.id.videoId) }}>{data.snippet.title}</div> </span>
+                            <div className='bg-[#1e1e1e] text-[#ffffff] h-[130px] p-6 font-[600]'>
+                                <span><div className='hover:cursor-pointer' onClick={() => { navigate("/videoes/" + data.snippet.channelId + "+" + data.id.videoId) }}>{data.snippet.title.substring(0, 55)}</div> </span>
                                 <div className='text-[#545554] mt-4 flex cursor-pointer' onClick={() => { navigate("/channel/" + data.snippet.channelId) }} ><div className='flex' >{data.snippet.channelTitle} <span className='ml-2 pt-1'><Tick /></span></div></div>
                             </div>
                         </div>
